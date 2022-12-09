db46e2382cc4fe0ee0bb5c0b6f0a1927d02ce9c1
Anna <annkrv80@yandex.ru>
2022-12-09 21:46:18 +0700

    Время заполнения ArrayList и LuenkedList

--- FirstProblem.java
+++ FirstProblem.java
@@ -3,36 +3,26 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 
 public class FirstProblem {
+
     public void arrayListTime(){
-        ArrayList<Integer> arrayList=new ArrayList<>();
-        Date start=new Date(0);
-        for(int i=0; i<1000;i++){
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        Date start = new Date(0);
+        for (int i=0; i < 300000; i++) {
             arrayList.add(i);
         }
-        Date end=new Date(0);
-        long duration=end.getTime()-start.getTime();
-        System.out.println("ArrayList " + duration);
+        Date end = new Date(0);
+        long duration = end.getTime() - start.getTime();
+        System.out.println("ArrayList: " + duration);
     }
 
     public void linkedListTime(){
-        LinkedList<Integer>linkedList = new LinkedList<>();
-        Date start=new Date(0);
-        for(int i=0;i<1000;i++){
+        LinkedList<Integer> linkedList = new LinkedList<>();
+        Date start = new Date(0);
+        for (int i=0; i < 300000; i++) {
             linkedList.add(i);
         }
-        Date end=new Date(0);
-        long duration=end.getTime()-start.getTime();
-        System.out.println("LinkedList "+ duration);
+        Date end = new Date(0);
+        long duration = end.getTime() - start.getTime();
+        System.out.println("LinkedList: " + duration);
     }
-    
 }
-
-/*public static void main(String[] args) {
-    FirstProblem firstProblem= new FirstProblem();
-    firstProblem.arrayListTime(); 
-    firstProblem.linkedListTime();
-     
- }*/
-
- 
- 
