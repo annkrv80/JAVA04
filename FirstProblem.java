2ed0f0224ccae14910b5f47a76707fd5fd481db9
Anna <annkrv80@yandex.ru>
2022-12-09 21:37:00 +0700

    Время заполнения ArrayList и LuenkedList

---
+++ FirstProblem.java
@@ -0,0 +1,38 @@
+import java.sql.Date;
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+public class FirstProblem {
+    public void arrayListTime(){
+        ArrayList<Integer> arrayList=new ArrayList<>();
+        Date start=new Date(0);
+        for(int i=0; i<1000;i++){
+            arrayList.add(i);
+        }
+        Date end=new Date(0);
+        long duration=end.getTime()-start.getTime();
+        System.out.println("ArrayList " + duration);
+    }
+
+    public void linkedListTime(){
+        LinkedList<Integer>linkedList = new LinkedList<>();
+        Date start=new Date(0);
+        for(int i=0;i<1000;i++){
+            linkedList.add(i);
+        }
+        Date end=new Date(0);
+        long duration=end.getTime()-start.getTime();
+        System.out.println("LinkedList "+ duration);
+    }
+    
+}
+
+/*public static void main(String[] args) {
+    FirstProblem firstProblem= new FirstProblem();
+    firstProblem.arrayListTime(); 
+    firstProblem.linkedListTime();
+     
+ }*/
+
+ 
+ 
